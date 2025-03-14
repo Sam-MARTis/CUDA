6fd703b31ae502987d515ade9bd066a1817ab742
Sam-MARTis <samanthmartis@gmail.com>
2025-03-14 14:38:54 +0530

    Added unified arrays code

---
+++ CUDA_Book_CUDA-For-Engineers/03-Chapter_From-Loops-To-Grids/dist_v2_cuda_unified/kernel.cu
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define N 64
+#define TPB 32
+
+__device__ float distance(float x1, float x2){
+    return sqrt((x2-x1)*(x2-x1));
+}
+
+__global__ void distanceKernel(float *d_out, float *d_in, float ref, int len){
+    const int i = blockIdx.x*blockDim.x + threadIdx.x;
+    const float x = d_in[i];
+    d_out[i] = distance(x, ref);
+    printf(" = %2d: dist from %f to %f is %f.\n", i, ref, x, d_out[i]);
+}
+
+__host__ void distanceArray(float *out, float *in, float ref, int len){
+    float *d_in = 0;
+    float *d_out = 0;
+
+    cudaMallocManaged(&d_in,  len*sizeof(float));
+    cudaMallocManaged(&d_out, len*sizeof(float));
+    
+    distanceKernel<<<N/TPB, TPB>>>(d_out, d_in, ref, N);
+    cudaDeviceSynchronize();
+
+    cudaFree(d_in);
+    cudaFree(d_out);
+}
